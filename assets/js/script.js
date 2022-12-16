3878974ca3ee265acd72f2075e42300569f88bd8
Seyed Mohsen Hatami <seyed.h.158@gmail.com>
2022-12-16 09:54:32 +0000

    Update assets/css/style.css, assets/js/script.js Deleted index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+let burger = document.getElementById("burger-menu")
+let navbar = document.getElementsByTagName("aside")
+burger.onclick = function(){
+    burger.classList.toggle("active-burger")
+    burger.classList.toggle("inactive-burger")
 
+    if(burger.classList.contains("active-burger")){
+        navbar[0].classList.add("full-aside")
+    }else {
+        navbar[0].classList.remove("full-aside")
+    }
+}
